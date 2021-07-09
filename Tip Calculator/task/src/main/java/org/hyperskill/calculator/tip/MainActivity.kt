94996e4e46d5dedc5277cf3a4e7badde2571af8d
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-07-08 23:27:45 -0400

    Stage 2 of 3 for JetBrains Academy - Kotlin Android - Tip Calculator project. Description
    Now that your application has the appropriate UI components, it needs to be able to tell when the user input changes: this way, it will know when to redo the calculation and show the updated tip amount. For now, we will use the EditText field to show the user input values.
    Objectives
    
    Add the appropriate event listeners to the EditText field and the Slider so that the program knows when the user provides input.
    
    Show the user input values in the TextView in the format Bill value: <X>, tip percentage: <P>% with the corresponding values instead of <X> and <P>.
    
    Using the event listeners, update TextView each time the user input changes. If the bill value is not provided (that is, EditText is empty), then TextView should disappear.
    
    MainActivity changes:
    • added val calculator that links to CalculatorViewModel
    • observe calculator.getResult to update the view named text_view
    • set doOnTextChanged for view edit_text, which calls calculator.billChange() with the updated data.
    • set addOnChangeListener for view slider, which calls calculator.percentChange() with the updated data.

--- Tip Calculator/task/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
+++ Tip Calculator/task/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
@@ -1,11 +1,30 @@
 package org.hyperskill.calculator.tip
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calculator: CalculatorViewModel by viewModels()
+
+        calculator.getResult.observe(this, { stringResult ->
+            findViewById<TextView>(R.id.text_view).text = stringResult
+        })
+
+        findViewById<EditText>(R.id.edit_text).doOnTextChanged { text, _, _, _ ->
+            calculator.billChange(text?.toString() ?: "")
+        }
+
+        findViewById<Slider>(R.id.slider).addOnChangeListener { _, value, _ ->
+            calculator.percentChange(value.toInt())
+        }
     }
 }
